4946b40d6602075164a7c497c7ab10a6ed330de6
AyanKR20 <161848150+AyanKR20@users.noreply.github.com>
2026-04-22 22:13:53 +0500

    privacy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/index.html",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+
   async rewrites() {
     return [
       {
