6800552bc6104874b735f80fbee3c5eb478c2d67
AyanKR20 <161848150+AyanKR20@users.noreply.github.com>
2026-04-22 21:56:51 +0500

    privacy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/privacypolicy",
+        destination: "/privacy.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
