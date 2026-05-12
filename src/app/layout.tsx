fb1dfd0945ba9e6a3359ec20cb7fe59177e1e423
AyanKR20 <161848150+AyanKR20@users.noreply.github.com>
2026-05-12 16:45:59 +0500

    views

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans, Playfair_Display } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
-import { PostHogProvider } from "@/components/PostHogProvider";
 import "./globals.css";
 
 const dmSans = DM_Sans({
@@ -41,10 +40,8 @@ export default function RootLayout({
       className={`${dmSans.variable} ${playfair.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <PostHogProvider>
-          {children}
-          <Analytics />
-        </PostHogProvider>
+        {children}
+        <Analytics />
       </body>
     </html>
   );
